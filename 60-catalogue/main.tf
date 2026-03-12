cad3f1b692bc599d41cba4f533b175ef1d5c1c25
Sivakumar Reddy M <info@joindevops.com>
2026-03-12 07:57:28 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -151,8 +151,8 @@ resource "aws_autoscaling_group" "catalogue" {
         local.common_tags
     )
     content {
-      key                 = each.key
-      value               = each.value
+      key                 = tag.key
+      value               = tag.value
       propagate_at_launch = true
     }
   }
