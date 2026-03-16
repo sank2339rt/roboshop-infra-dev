ea9e5d522dec4ec6f794c4063111a85a7b93360b
Sivakumar Reddy M <info@joindevops.com>
2026-03-16 09:16:59 +0530

    terraform

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -8,4 +8,5 @@ locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   user_sg_id = data.aws_ssm_parameter.user_sg_id.value
   backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+  frontend_alb_sg_id = data.aws_ssm_parameter.frontend_alb_sg_id.value
 }
