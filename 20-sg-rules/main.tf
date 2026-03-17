8a54d6509a7f2dcf6ee133945b52c31415d1d262
Sivakumar Reddy M <info@joindevops.com>
2026-03-17 08:52:58 +0530

    terraform

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -291,6 +291,16 @@ resource "aws_security_group_rule" "backend_alb_frontend" {
 }
 
 # Frontend
+resource "aws_security_group_rule" "frontend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  # Where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id = local.frontend_sg_id
+}
+
 resource "aws_security_group_rule" "frontend_frontend_alb" {
   type              = "ingress"
   from_port         = 80
